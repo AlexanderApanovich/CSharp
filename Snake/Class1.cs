637685800289f175eccf7d88a0bb52fb66d0f2e5
Alexander <al29051999@mail.ru>
2018-09-09 14:46:05 +0300

    C#

---
+++ Snake/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Field
+    {
+        public int Width;
+        public int Height;
+
+        public Field(int Width, int Height)
+        {
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public void Render()
+        {
+            for (int y = 0; y <= Height; y++)
+            {
+                if (y == 0 || y == Height)
+                {
+                    for (int x = 0; x < Width; x++) Console.Write("#");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.SetCursorPosition(0, y);
+                    Console.Write("#");
+                    Console.SetCursorPosition(Width - 1, y);
+                    Console.Write("#");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Width = 15;
+            int Height = 10;
+
+            int Score=0;
+
+            Console.SetWindowSize(Width, Height);
+            Console.SetBufferSize(Width, Height);
+            Console.CursorVisible = false;
+
+            Snake snake = new Snake();
+            Food food = new Food();
+            ConsoleKey key;
+
+            snake.Render();
+            food.CreateFood(Width, Height, snake);
+            food.Render();
+
+
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    if (snake.FoodCollider(food))
+                    {
+                        food.CreateFood(Width, Height, snake);
+                        Score++;
+                    }
+
+                    snake.Push(snake.SnakeHead);
+                    if (snake.Length>=Score)
+                    {
+                        snake.RemoveFirst();
+                    }
+
+                    Thread.Sleep(500);
+                    switch (snake.Moving)
+                    {
+                        case "left": snake.SnakeHead.X--; break;
+                        case "right": snake.SnakeHead.X++; break;
+                        case "up": snake.SnakeHead.Y--; break;
+                        case "down": snake.SnakeHead.Y++; break;
+                    }
+                    
+                    Console.Clear();
+                    food.Render();
+                    snake.Render();
+                    
+                }
+
+                key = Console.ReadKey().Key;
+
+                snake.SnakeHeadMove(key);
+
+                Thread.Sleep(500);
+                Console.Clear();
+                food.Render();
+                snake.Render();
+                
+            } while (key != ConsoleKey.Escape);
+
+
+        }
+    }
+}
